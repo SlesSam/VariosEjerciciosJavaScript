30d7e9f33c349cc55a9794e8884f44d399cc1410
SlesSam <lesy.sam9422@gmail.com>
2022-06-21 16:38:02 +0200

    modificado 2

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -2,11 +2,29 @@
 * 2. Ejercicio 
 Dado un array de valores, devolver un array truthy (sin valores nulos, vacíos, no números, indefinidos o falsos)
 */
+
 const arrDirty = [NaN, 0, 5, false, -1, '',undefined, 3, null, 'test']
 
-let incl = arrDirty.includes('test',0)
+//version 1 
+function istruthy(arrDirty){
+var arraNew=[]
+  arrDirty.forEach((va)=>{
+    if(va){
+      arraNew.push(va)
+    }
+    
+  })
+  return arraNew;
+  
+}
+console.log('----un version 1----')
+console.log(istruthy(arrDirty))
 
 
-console.log(incl) 
 
+//version 2
+const truly = arrDirty.filter(element => element)
+
+console.log('----un version 2----')
+console.log(truly)
 
